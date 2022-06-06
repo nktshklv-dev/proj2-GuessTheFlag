4db5411c785e42a9ae188af24ed0015b40c38040
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-06 09:18:35 +0300

    simple functional for randomizing countries

--- Guess The Flag/ViewController.swift
+++ Guess The Flag/ViewController.swift
@@ -13,9 +13,32 @@ class ViewController: UIViewController {
     @IBOutlet var buttonTwo: UIButton!
     @IBOutlet var buttonThree: UIButton!
     
+    var countries: [String] = []
+    var score = 0
+    var correctAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countries.append(contentsOf: ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"])
+        askQuestion()
+        buttonOne.layer.borderWidth = 2
+        buttonTwo.layer.borderWidth = 2
+        buttonThree.layer.borderWidth = 2
+        
+        buttonThree.layer.borderColor = UIColor.lightGray.cgColor
+        buttonTwo.layer.borderColor = UIColor.lightGray.cgColor
+        buttonOne.layer.borderColor = UIColor.lightGray.cgColor
+        
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
+        buttonTwo.imageView?.image = UIImage(named: countries[1])
+        buttonThree.imageView?.image = UIImage(named: countries[2])
+        
+        title = countries[correctAnswer].uppercased()
     }
 
 
